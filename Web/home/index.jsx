86da4e5fe300fa4b842a8e06771364f475ebc3e9
Afnan <temptemporary362@gmail.com>
2025-06-12 21:49:05 +0600

    Update index.jsx

--- Web/home/index.jsx
+++ Web/home/index.jsx
@@ -68,11 +68,11 @@ const Footer = () => (
                 <p>Github</p>
             </div>
             <div class="icon-with-label">
-                <a href="https://discord.gg/KKjw6U7Stw"><img src="../Discord-logo.svg" alt="My Discord Server" oncontextmenu="return false" /></a>
+                <a href="https://discord.gg/KKjw6U7Stw"><img src="../assets/Discord-logo.svg" alt="My Discord Server" oncontextmenu="return false" /></a>
                 <p>Discord</p>
             </div>
             <div class="icon-with-label">
-                <a href="https://github.com/repo/form"><img src="../Form-logo.svg" alt="My Contact Form" oncontextmenu="return false" /></a>
+                <a href="https://github.com/repo/form"><img src="../assets/Form-logo.svg" alt="My Contact Form" oncontextmenu="return false" /></a>
                 <p>Contact form</p>
             </div>
         </div>
